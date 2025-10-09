ccc516ff7a1c8213355412f8c3c43bbde83a8c99
VEERUPAMARTHI <147476610+VEERUPAMARTHI@users.noreply.github.com>
2025-10-09 22:48:14 +0530

    Time: 701 ms (5.09%), Space: 0B (100%) - LeetHub

---
+++ 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
@@ -0,0 +1,10 @@
+/* Write your PL/SQL query statement below */
+WITH cte AS   
+(
+    SELECT machine_id,process_id,MAX(timestamp)-MIN(timestamp) AS timestamp
+    FROM activity
+    GROUP BY machine_id,process_id
+)
+SELECT machine_id,ROUND(AVG(timestamp),3) AS processing_time
+FROM cte
+GROUP BY machine_id;
