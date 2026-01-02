ba1898a47399c041541fbeef4e836fc6dc5f1f63
VEERUPAMARTHI <147476610+VEERUPAMARTHI@users.noreply.github.com>
2026-01-02 22:04:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Search array with adjacent diff at most k/search-array-with-adjacent-diff-at-most-k.cpp
@@ -0,0 +1,15 @@
+// x is the element to be searched in arr[0..n-1]
+// such that all elements differ by at-most k.
+
+class Solution {
+  public:
+    int findStepKeyIndex(vector<int>& arr, int k, int x) {
+        // code here
+        for(int i=0;i<arr.size();i++){
+            if(x==arr[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
