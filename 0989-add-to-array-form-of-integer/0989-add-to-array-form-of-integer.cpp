5bd3d9124029ccfb21eeda378c409e2eeb19d77b
VEERUPAMARTHI <147476610+VEERUPAMARTHI@users.noreply.github.com>
2026-01-07 23:08:56 +0530

    Time: 0 ms (100%), Space: 31.6 MB (81.39%) - LeetHub

---
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> addToArrayForm(vector<int>& num, int k) {
+        int i = num.size() - 1;
+        int carry = 0;
+        while(i >= 0 || k > 0 || carry){
+            int sum = carry;
+            if(i >= 0){
+                sum += num[i];
+            }
+            if(k > 0){
+                sum += (k % 10);
+                k /= 10;
+            }
+            int digit = sum % 10;
+            carry = sum/10;
+            if(i >= 0){
+                num[i] = digit;
+                i--;
+            }
+            else{
+                num.insert(num.begin(), digit);
+            }
+        }
+        return num;
+    }
+};
